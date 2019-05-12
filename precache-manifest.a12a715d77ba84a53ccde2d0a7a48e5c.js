03ff5be769cf6a7e5969bfb7a8bfaaf4747cd301
Hope Myers <hopemyers@Hopes-MacBook-Air.local>
2019-05-12 20:04:32 +0800

    Updates

---
+++ precache-manifest.a12a715d77ba84a53ccde2d0a7a48e5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8922bac9e3979da534cd6c1c578307",
+    "url": "/slanguage/index.html"
+  },
+  {
+    "revision": "2e4d189082b6ae80f2a9",
+    "url": "/slanguage/static/css/main.70e622d3.chunk.css"
+  },
+  {
+    "revision": "cdea3b3cc4563c90e40a",
+    "url": "/slanguage/static/js/2.e8151771.chunk.js"
+  },
+  {
+    "revision": "2e4d189082b6ae80f2a9",
+    "url": "/slanguage/static/js/main.ea363d78.chunk.js"
+  },
+  {
+    "revision": "6bfd76104b74414e3cde",
+    "url": "/slanguage/static/js/runtime~main.d0919361.js"
+  }
+]);
